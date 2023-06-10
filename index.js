ecc97e1dee1fd6bc054dbfd13d63bbd3c344ea62
br25 <md.ashrafulislamrobin12345@gmail.com>
2023-06-10 15:28:13 +0600

    Query data on terminal

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
+const fs = require('fs');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const { buildSchema, execute, parse } = require('graphql');
+
+const userData = fs.readFileSync('user.json', 'utf8');
+const users = JSON.parse(userData);
 
 const app = express();
 const port = 3001;
 
 const schema = buildSchema(`
+  type User {
+    first_name: String
+    last_name: String
+    email: String
+    gender: String
+    ip_address: String
+  }
+
   type Query {
-    name: String
-    age: Int
+    getUser(id: Int!): User
+    getAllUsers: [User]
   }
 `);
 
 const root = {
-    name: () => 'Robin',
-    age: () => 26
+    getUser: (args) => {
+        const id = args.id;
+        return users.find(user => user.id === id);
+    },
+    getAllUsers: () => {
+        return users;
+    }
 };
+
+const query = `
+  query GetUser($userId: Int!) {
+    getUser(id: $userId) {
+      first_name
+      last_name
+      email
+    }
+  }
+`;
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
@@ -26,3 +53,13 @@ app.use('/graphql', graphqlHTTP({
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}/graphql`);
 });
+
+// Run the query
+const result = execute({
+    schema: schema,
+    document: parse(query),
+    rootValue: root,
+    variableValues: { userId: 1 }
+});
+
+console.log(result)
